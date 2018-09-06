dc88ddddc93642c1a213cef47bb0cd64b7295ebb
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-06 06:48:32 -0400

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.write('\
+<script src="js/cookies.js"></script>\
+    <script src="js/mode.js"></script>\
+    <script src="js/randomTagline.js"></script>\
+	  \
+    <button id="modeButton" class="night"></button>\
+    <script>initModeButton();</script>\
+	\
+    <h1>Monya Monya</h1>\
+    <p style="font-size:80%" id="tagline"></p>\
+<script>document.getElementById("tagline").innerText = getRandomTagline();</script>';\
+\
+');
