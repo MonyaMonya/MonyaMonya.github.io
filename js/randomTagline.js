54df381c11bc9bb95a0b3d77e2fa88e7ddd72ac5
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-06 06:30:09 -0400

    Update randomTagline.js

--- js/randomTagline.js
+++ js/randomTagline.js
@@ -1,9 +1,24 @@
 var randomTaglines = [
-"Red",
-"Orange",
-"Yellow",
-"Green",
-"Blue"
+  "Sitting out in the rain",
+  "Your ad goes here",
+  "Alt size fiction",
+  "Awake under the covers",
+  "Trapped by hungry cats",
+  "Don't wake me up",
+  "Kissing after dark",
+  "An anhedonic joy",
+  "The zoo is closed today",
+  "Folding an origami noose",
+  "Bouquet of wet roses",
+  "Cold and spicy rice",
+  "At the top of Pascal's Triangle",
+  "Let's talk over donuts",
+  "Don't hold my hand so tight",
+  "Sleeping in oversized clothes",
+  "Please stop",
+  "I'm sorry I hurt you",
+  "Trickster and minuteman",
+  "Yesterday, today, tomorrow"
 ];
 
 function getRandomTagline() {
