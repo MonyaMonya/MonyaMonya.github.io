a98a29ad428d6e3a4e5ff1b98f2cc3b6afa3ab82
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-06 04:59:25 -0400

    Create mode.js

---
+++ js/mode.js
@@ -0,0 +1,35 @@
+var modeName = getCookie("mode");
+setMode();
+
+function setMode() {
+  if(modeName != "day")
+  {
+	modeName = "night";
+  }
+  document.getElementById("body").className = modeName;
+  if(modeName == "day")
+  {
+     var elem = document.getElementById("modeButton");
+    elem.innerText = "☾";
+    elem.className = modeName;
+  }
+  else
+  {
+     var elem = document.getElementById("modeButton");
+    elem.innerText = "☀";
+    elem.className = modeName;
+  }
+  setCookie("mode", modeName, 365);
+}
+
+function changeModeButton() {
+  if(modeName != "night")
+  {
+	modeName = "night";
+  }
+  else
+  {
+    modeName = "day";
+  }
+  setMode();
+}
