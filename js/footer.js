6894ab2469621e62d085ac5c533dfd56272451c0
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-07 22:31:09 -0400

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,25 @@
+document.write('\
+<canvas id="footerCanvas" width=400 height=200></canvas>\
+\
+');
+
+var img = new Image,
+    canvas = document.getElementById('footerCanvas'),
+    ctx = canvas.getContext('2d');
+
+/// change to this color
+ctx.fillStyle = '#00F';
+
+/// load image, when ready render it with new color
+img.onload = render;
+img.src = 'content/stepladder.gif';
+
+function render() {
+    /// this composite mode clears the canvas as well
+    ctx.globalCompositeOperation = 'copy';
+    ctx.drawImage(img, 0, 0);
+
+    /// this mode fills in whatever, in the image
+    ctx.globalCompositeOperation = 'source-in';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
