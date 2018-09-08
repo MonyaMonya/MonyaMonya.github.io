89accb533b8fbbaa86c065f470ebc59eafc20fba
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-07 23:35:20 -0400

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,25 +1,7 @@
 document.write('\
+<script src="js/animatedCanvas.js"></script>\
 <canvas id="footerCanvas"></canvas>\
 \
 ');
 
-var img = new Image,
-    canvas = document.getElementById('footerCanvas'),
-    ctx = canvas.getContext('2d');
-
-/// change to this color
-ctx.fillStyle = '#00F';
-
-/// load image, when ready render it with new color
-img.onload = render;
-img.src = 'content/stepladder.gif';
-
-function render() {
-    /// this composite mode clears the canvas as well
-    ctx.globalCompositeOperation = 'copy';
-    ctx.drawImage(img, 0, 0);
-
-    /// this mode fills in whatever, in the image
-    ctx.globalCompositeOperation = 'source-in';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+setAnimatedCanvas(footerCanvas, "content/stepladderfull.png", 1183, 577, 3, 200, '#00F');
