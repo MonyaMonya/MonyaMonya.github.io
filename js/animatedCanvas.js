0d3f0777bec5ec015fc2c56cee880e9cac4add35
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-07 23:55:41 -0400

    Update animatedCanvas.js

--- js/animatedCanvas.js
+++ js/animatedCanvas.js
@@ -19,15 +19,15 @@ function setAnimatedCanvas(canvasName, image, frameWidth, frameHeight, frameCoun
   function animate() {
   
     /// this composite mode clears the canvas as well
-    ctx.globalCompositeOperation = 'copy';
+    context.globalCompositeOperation = 'copy';
 
     //draw each frame + place them in the middle
     context.drawImage(myImage, shift, 0, frameWidth, frameHeight,
                       0, 0, frameWidth, frameHeight);
 
     /// this mode fills in whatever, in the image
-    ctx.globalCompositeOperation = 'source-in';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    context.globalCompositeOperation = 'source-in';
+    context.fillRect(0, 0, canvas.width, canvas.height);
     
     shift += frameWidth + 1;
 
