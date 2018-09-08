f35c1f0e1a87086ed83df1a33f68ed3d0f2fa18b
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-08 01:41:53 -0400

    Update header.js

--- js/header.js
+++ js/header.js
@@ -3,11 +3,21 @@ document.write('\
     <script src="js/mode.js"></script>\
     <script src="js/randomTagline.js"></script>\
 	  \
+      <div id="header">\
     <button id="modeButton" class="night"></button>\
     <script>initModeButton();</script>\
 	\
     <h1>Monya Monya</h1>\
     <p style="font-size:80%" id="tagline"></p>\
 <script>document.getElementById("tagline").innerText = getRandomTagline();</script>\
+\
+    <table style="width:100%; text-align:center">\
+      <tr>\
+        <td style="width:50%"></td>\
+        <td>Stories</td>\
+        <td>About</td>\
+      </tr>\
+    </table>\
+      </div><!-- #header -->\
 \
 ');
