65fa6929c8e250f841c62ce29ad07d9ded4316e5
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-08 01:10:19 -0400

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,9 @@
 document.write('\
 <script src="js/animatedCanvas.js"></script>\
+<div id="footerCanvas"></canvas>\
 <canvas id="footerCanvas"></canvas>\
 \
-<script>setAnimatedCanvas("footerCanvas", "content/stepladderfull.gif", 1183, 577, 3, 200, "#00F");</script>\
+<script>setAnimatedCanvas("footerCanvas", "content/stepladderfull.gif", 1183, 577, 3, 200, "#CCC");</script>\
 \
 ');
 
