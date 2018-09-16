80c0094191aada4bca8042749980b0df717c959c
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-16 06:01:48 -0400

    Update listings.js

--- stories/listings.js
+++ stories/listings.js
@@ -1,4 +1,4 @@
 var listingsTable =  [
-  ["Isle For Two", "IsleForTwo", 1, "Two girls spend a quiet moment together before parting ways. A quick story about the distance that can come from growing up too fast. Written for GrowthJuly18."]
-  ["Test Story 1", "TestStory1", 3, "Chapter 1, Test text goes here.", "Chapter two, hot tub jugaloo.", "The final chapter."]
+  ["Isle For Two", "IsleForTwo", 1, {"Two girls spend a quiet moment together before parting ways. A quick story about the distance that can come from growing up too fast. Written for GrowthJuly18."}]
+  ["Test Story 1", "TestStory1", 3, { "Chapter 1, Test text goes here.", "Chapter two, hot tub jugaloo.", "The final chapter."}]
 ];
