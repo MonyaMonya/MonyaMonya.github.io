e91630d8d30706f2d04ba92838d22bf9ae040f33
MonyaMonya <41931293+MonyaMonya@users.noreply.github.com>
2018-09-16 05:07:43 -0400

    Update listings.js

--- stories/listings.js
+++ stories/listings.js
@@ -1,4 +1,4 @@
 var listingsTable =  [
-  ["Test Story 1", "TestStory1", "This is a test story, no need to worry too much about it now : )", 3],
-  ["Test Story 1", "TestStory1", "This is a test story, no need to worry too much about it now : )", 3]
+  ["Isle For Two", "IsleForTwo", 1, "Two girls spend a quiet moment together before parting ways. A quick story about the distance that can come from growing up too fast. Written for GrowthJuly18."]
+  ["Test Story 1", "TestStory1", 3, "Chapter 1, Test text goes here.", "Chapter two, hot tub jugaloo.", "The final chapter."]
 ];
